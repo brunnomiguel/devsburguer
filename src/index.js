856e1730f639a39c228732b29b1262d26c16c95e
Brunno Miguel <miguel.brunno06@gmail.com>
2022-07-12 11:41:40 -0300

    build: created initial structure and installed libs

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import Providers from "./providers";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Providers>
+        <App />
+      </Providers>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
